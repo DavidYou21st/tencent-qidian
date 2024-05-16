195514b453dce34c839ec6626f627efd4eb5de2a
david you <youlinmi@iwgame.com>
2024-05-16 21:00:26 +0800

    package tencent qidian sdk

--- vendor_old/composer/ClassLoader.php
+++ vendor_old/composer/ClassLoader.php
@@ -37,7 +37,7 @@ namespace Composer\Autoload;
  *
  * @author Fabien Potencier <fabien@symfony.com>
  * @author Jordi Boggiano <j.boggiano@seld.be>
- * @see    http://www.php-fig.org/psr/psr-0/
+   * @see    http://www.php-fig.org/psr/psr-0/
  * @see    http://www.php-fig.org/psr/psr-4/
  */
 class ClassLoader
